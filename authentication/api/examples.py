6dc7f2f1f04fe285507194a3c07710b2c74197ac
Kip Parker <kip@fifthcontinent.io>
2025-02-24 15:30:40 +0000

    Remove unused models and examples

--- authentication/api/examples.py
+++ authentication/api/examples.py
@@ -2,11 +2,14 @@ from pydantic.config import JsonDict
 
 
 ORY_CLIENT_ID = "abc123-addefg-4e2f-a8e3-cbd5f6459c30"
+DIRECTORY_CLIENT_ID = (
+    "https://registry.core.pilot.trust.ib1.org/scheme/perseus/application/test-suite"
+)
 
 CLIENT_CERTIFICATE = "-----BEGIN%20CERTIFICATE-----%0AMIIDkzCCAnugAwIBAgIUerCGLrDY6aCYaB6nj9HivLJQtCAwDQYJKoZIhvcNAQEL%0ABQAwVzELMAkGA1UEBhMCR0IxDzANBgNVBAgMBkxvbmRvbjETMBEGA1UECgwKUGVy%0Ac2V1cyBDQTEiMCAGA1UEAwwZcGVyc2V1cy1kZW1vLWZhcGkuaWIxLm9yZzAeFw0y%0ANDAxMzAxNTA2NDRaFw0yNTAxMjkxNTA2NDRaMGwxCzAJBgNVBAYTAkdCMQ8wDQYD%0AVQQIDAZMb25kb24xITAfBgNVBAoMGFBlcnNldXMgRGVtbyBBY2NvdW50YW5jeTEp%0AMCcGA1UEAwwgcGVyc2V1cy1kZW1vLWFjY291bnRhbmN5LmliMS5vcmcwggEiMA0G%0ACSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQC06LPNmROgBhDrfpUnfYaoqMENiqcy%0ArbqMGGWiRtRQO87ngL96iCMi30kPQjg4kD8PK49522ZZJ%2BVTqErtD6reS5y6mWkB%0AydsoWRKTi7UyHdyyK9D%2FHc%2FxK1JtFwlAZd4pMaN6KWJLEEfvmZxhOI5pfzKwi6Jj%0A%2BOfedpVJSAeXkI6CYY6qq33KRI4KQau5E7PbzbcNYICvLj1Vhs2bUz8a0tOJG5r8%0A06MCr%2FtteTYMjbb6x3yVlSL3b3LOtj4n8RCYyLlQ3S4ni1MSyHdnqngUGGyrnSVI%0AJF3lR9xJ6AK96RNgiRSzjCWOjOFZjnpuFlGa%2FXxn7buIrDehuKlmVDm3AgMBAAGj%0AQjBAMB0GA1UdDgQWBBRgUt%2BadzZuWnp9bEQLx6qhSm0T%2BTAfBgNVHSMEGDAWgBQP%0Ad2ICahMnEqfEp%2FLCaoBcGQufojANBgkqhkiG9w0BAQsFAAOCAQEArGVkNNfH9Zct%0Ak68YUrky3jPc3L714CjsW3l7yCWCqmkuB6VWIggNqltdKQDzdXDIwFLmN%2Fi7D57K%0AFqDaQboKUumeF3vsIi4LYlRqwGTuN7uGTghcqrpPozM7m%2BYTdPObY%2F8FtL6MqmqJ%0Adv61MYERRl3iLuR6UIbBaQr4YvgThe9WGotqknFOyxrD1yuunlYutOQpF2tXR8hk%0AES5XhvdLQfiuGStM4MPB0%2FWlMwc7mVge5aOVOixJeB0yGNmSSJWeEMQB0ETW3BbF%0AySdR2NroAWqouPWaJMZIpjxldeeTOBmc8k6%2BDLBvRIFcDUgpuLfaoAFTZKtkWbao%0A2NHw6nQAMQ%3D%3D%0A-----END%20CERTIFICATE-----%0A"
 CLIENT_PUSHED_AUTHORIZATION_REQUEST: JsonDict = {
     "response_type": "code",
-    "client_id": ORY_CLIENT_ID,
+    "client_id": DIRECTORY_CLIENT_ID,
     "redirect_uri": "https://mobile.example.com/cb",
     "code_challenge": "W78hCS0q72DfIHa...kgZkEJuAFaT4",
     "code_challenge_method": "S256",
@@ -24,10 +27,6 @@ PUSHED_AUTHORIZATION_RESPONSE: JsonDict = {
     "request_uri": "urn:ietf:params:oauth:request_uri:UymBrux4ZEMrBRKx9UyKyIm98zpX1cHmAPGAGNofmm4",
 }
 
-AUTHORIZATION_REQUEST: JsonDict = {
-    "client_id": ORY_CLIENT_ID,
-    "request_uri": "urn:ietf:params:oauth:request_uri:UymBrux4ZEMrBRKx9UyKyIm98zpX1cHmAPGAGNofmm4",
-}
 
 AUTHORIZATION_RESPONSE: JsonDict = {
     "message": "Authorisation code request issued",
@@ -53,24 +52,4 @@ TOKEN_RESPONSE: JsonDict = {
         "o5lPKehvIHyViZgRg4CY_ZGmnyFooG4FCwlZxu-QOTtaDCffCsuCdz4GqknTA"
     ),
     "refresh_token": "tXZjYfoK35I-djg9V3n6s58zsrVqRIzTNMXKIS_wkj8",
-}
-
-INTROSPECTION_REQUEST: JsonDict = {
-    "token": "SUtEVc3Tj3D3xOdysQtssQxe9egAhI4fimexNVMjRyU",
-    "client_certificate": CLIENT_CERTIFICATE,
-}
-INTROSPECTION_FAILED_RESPONSE: JsonDict = {"active": False}
-INTROSPECTION_RESPONSE = {
-    "aud": [],
-    "client_id": ORY_CLIENT_ID,
-    "exp": 1713285925,
-    "ext": {},
-    "iat": 1713282325,
-    "iss": "https://vigorous-heyrovsky-1trvv0ikx9.projects.oryapis.com",
-    "jti": "cca497f5-f3b0-4c81-b873-7f97a74cfcda",
-    "nbf": 1713282325,
-    "scp": ["profile", "offline_access"],
-    "sub": "d6fd6e1c-a10e-40d8-aa2b-9606f3d34d3c",
-    "cnf": {"x5t#S256": "k6Joc_TbRIm_vIQyrWcMTIVz_QZmR0JReGASWRcLdnQ"},
-    "active": True,
 }
--- authentication/api/models.py
+++ authentication/api/models.py
@@ -45,18 +45,6 @@ class PushedAuthorizationResponse(BaseModel):
     }
 
 
-class AuthorizationRequest(BaseModel):
-    request_uri: str
-    client_id: str
-    model_config = {
-        "json_schema_extra": {
-            "examples": [
-                examples.AUTHORIZATION_REQUEST,
-            ]
-        }
-    }
-
-
 class AuthorizationResponse(BaseModel):
     """ """
 
@@ -86,93 +74,5 @@ class TokenResponse(BaseModel):
     model_config = {"json_schema_extra": {"examples": [examples.TOKEN_RESPONSE]}}
 
 
-class IntrospectionRequest(BaseModel):
-    token: str
-    client_certificate: str
-    model_config = {
-        "json_schema_extra": {
-            "examples": [
-                examples.INTROSPECTION_REQUEST,
-            ]
-        }
-    }
-
-
-class IntrospectionFailedResponse(BaseModel):
-    active: bool
-    model_config = {
-        "json_schema_extra": {
-            "examples": [
-                examples.INTROSPECTION_FAILED_RESPONSE,
-            ]
-        }
-    }
-
-
 class Cnf(BaseModel):
     x5t_S256: str = Field(alias="x5t#S256")
-
-
-class IntrospectionResponse(BaseModel):
-    aud: list[str]
-    client_id: str
-    exp: int
-    ext: dict[str, Any]
-    iat: int
-    iss: str
-    jti: str
-    nbf: int
-    scp: list[str]
-    sub: str
-    cnf: Cnf
-    active: bool
-    model_config = {
-        "json_schema_extra": {
-            "examples": [
-                {
-                    "aud": [],
-                    "client_id": "f67916ce-de33-4e2f-a8e3-cbd5f6459c30",
-                    "exp": 1713285925,
-                    "ext": {},
-                    "iat": 1713282325,
-                    "iss": "https://vigorous-heyrovsky-1trvv0ikx9.projects.oryapis.com",
-                    "jti": "cca497f5-f3b0-4c81-b873-7f97a74cfcda",
-                    "nbf": 1713282325,
-                    "scp": ["profile", "offline_access"],
-                    "sub": "d6fd6e1c-a10e-40d8-aa2b-9606f3d34d3c",
-                    "cnf": {"x5t#S256": "k6Joc_TbRIm_vIQyrWcMTIVz_QZmR0JReGASWRcLdnQ"},
-                    "active": True,
-                }
-            ]
-        }
-    }
-
-
-# Models for user authentication demo
-
-
-class UserToken(BaseModel):
-    access_token: str
-    token_type: str
-
-
-class UserTokenData(BaseModel):
-    username: str | None = None
-
-
-class User(BaseModel):
-    username: str
-    email: str | None = None
-    full_name: str | None = None
-    disabled: bool | None = None
-
-
-class UserInDB(User):
-    hashed_password: str
-
-
-class Consent(BaseModel):
-    scopes: list[str]
-    model_config = {
-        "json_schema_extra": {"examples": [{"scopes": ["openid", "profile"]}]}
-    }
